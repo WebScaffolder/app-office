dcd36cf7030bab7f994ad9c6c1ca4ba3208d692f
iClouds App Stall <inlaysoft@yahoo.com>
2022-10-29 09:44:46 +0100

    office

--- polyfills-es5.js
+++ polyfills-es5.js
@@ -14661,9 +14661,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\office_new\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
-__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\office_new\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
-module.exports = __webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\office_new\src\polyfills.ts */"./src/polyfills.ts");
+__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\iClouds App\office\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
+__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\iClouds App\office\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es5-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-jit-polyfills.js");
+module.exports = __webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\iClouds App\office\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- polyfills.js
+++ polyfills.js
@@ -6931,8 +6931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\office_new\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\office_new\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\iClouds App\office\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\SAAS APP ARCHITECTURE\MAIN APPS\iClouds App\office\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
